03af3d8b8c112208f36567eb806a32615203db5b
krishna7275 <kpy.yt9833067464@gmail.com>
2025-11-08 20:08:25 +0530

    complte a show and new route and ejs page

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
 
 //root route
 app.get("/",(req,res) => {
@@ -46,7 +47,26 @@ app.get("/",(req,res) => {
 app.get("/listings", async(req,res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", {allListings});
-})
+});
+
+//Add route
+app.get("/listings/new", (req,res) => {
+    res.render("listings/new.ejs");
+});
+
+//show route
+app.get("/listings/:id", async(req,res) => {
+    let {id} = req.params
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
+
+//create route
+app.post("/listings",async (req,res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
 
 app.listen(8080, () => {
 console.log("Server is listing at 8080");
